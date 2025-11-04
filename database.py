83e6df3a9be183d41e3f4d5a04cffc089ca4bfbd
Brunoreit <bruno.rf2@puccampinas.edu.br>
2025-11-04 15:51:52 -0300

    def:salvar_mensagem

--- database.py
+++ database.py
@@ -2,14 +2,32 @@ import os
 from dotenv import load_dotenv
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from datetime import datetime
 
 load_dotenv()
 uri = os.getenv("MONGODB_URI")
-
-client = MongoClient(uri, Server_api=ServerApi('1'))
+client = MongoClient(uri, server_api=ServerApi('1'))
 
 try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
+    db = client['chat']
+    mensagens_collection = db['mensageria']
+
 except Exception as e:
-    print(e)
+    print(f"Erro ao selecionar coleção: {e}")
+
+
+def salvar_mensagem(remetente, destinatario, mensagem_cifrada):
+    try:
+        mensagens_collection.insert_one(
+            {
+                "remetente": remetente,
+                "destinatario": destinatario,
+                "conteudo_mensagem": mensagem_cifrada,
+                "status": "nova",
+                "timestamp": datetime.now()
+            }
+        )
+        print(f"Mensagem salva no banco de dados.")
+
+    except Exception as e:
+        print(f"Falha ao salvar mensagem no banco: {e}")    
